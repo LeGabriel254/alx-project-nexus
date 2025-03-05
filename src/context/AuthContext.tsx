20f94a8dc0911d672630b1043464e4f67435e31c
Leon Gabriel <g73346920@gmail.com>
2025-03-06 00:27:04 +0300

    Chored:Fetch the current session and set the user state.

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -2,46 +2,58 @@ import { User } from "@supabase/supabase-js";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { supabase } from "../supabase-client";
 
+// Define the structure of the authentication context
 interface AuthContextType {
   user: User | null;
   signInWithGitHub: () => void;
   signOut: () => void;
 }
 
-const AuthContext = createContext<AuthContextType | undefined>(undefined)
+// Create an authentication context
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
-    supabase.auth.getSession().then(({data: {session } }) => {
-      setUser(session?.user ?? null)
-    })
-  }, [])
+    // Fetch the current session and set the user state
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user ?? null);
+    });
 
+    // Listen for authentication state changes
+    const { data: listener } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user ?? null);
+    });
+
+    // Cleanup function to unsubscribe from auth state changes
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
+
+  // Function to sign in using GitHub OAuth
   const signInWithGitHub = () => {
     supabase.auth.signInWithOAuth({ provider: "github" });
-  }
+  };
 
+  // Function to sign out the user
   const signOut = () => {
     supabase.auth.signOut();
-  }
-
+  };
 
   return (
-  <AuthContext.Provider value={{user, signInWithGitHub, signOut}}>
-    {""}
-    {children}{""}
-  </AuthContext.Provider>
-  )
-}
+    <AuthContext.Provider value={{ user, signInWithGitHub, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
+// Custom hook to use authentication context
 export const useAuth = (): AuthContextType => {
   const context = useContext(AuthContext);
-  if(context === undefined) {
-    throw new Error("useAuth must be used within the AuthProvider")
+  if (context === undefined) {
+    throw new Error("useAuth must be used within the AuthProvider");
   }
-  return context
-
-}
+  return context;
+};
