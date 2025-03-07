d12ead6dbd407aa0daece8e65504b37ae846c364
Leon Gabriel <g73346920@gmail.com>
2025-03-07 16:14:09 +0300

    Chored:Header modified

--- src/components/PostItem.tsx
+++ src/components/PostItem.tsx
@@ -13,9 +13,9 @@ export const PostItem = ({ post }: Props) => {
         <div className="w-80 h-76 bg-[rgb(24,27,32)] border border-[rgb(84,90,106)] rounded-[20px] text-white flex flex-col p-5 overflow-hidden transition-colors duration-300 group-hover:bg-gray-800">
           {/* Header: Avatar and Title */}
           <div className="flex items-center space-x-2">
-            {post.avatar_url ? (
+            {post.image_url ? (
               <img
-                src={post.avatar_url}
+                src={post.image_url}
                 alt="User Avatar"
                 className="w-[35px] h-[35px] rounded-full object-cover"
               />
@@ -30,9 +30,9 @@ export const PostItem = ({ post }: Props) => {
           </div>
 
           {/* Image Banner */}
-          <div className="mt-2 flex-1">
+          <div className="mt-4 flex-1">
             <img
-              src={post.image_url}
+              src={post.avatar_url}
               alt={post.title}
               className="w-full rounded-[20px] object-cover max-h-[150px] mx-auto"
             />
