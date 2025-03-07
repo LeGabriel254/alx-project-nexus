a8193f62e137e3def3fae82c328878abdacc7f44
Leon Gabriel <g73346920@gmail.com>
2025-03-07 16:13:22 +0300

    Chored:wrap our app with routes to manage navigation and ensure that different components or pages are rendered based on the URL

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@ import { Routes, Route } from 'react-router'
 import './App.css'
 import { Home } from './pages/Home'
 import { Navbar } from './components/Navbar'
-import { CreatPostPage } from './pages/CreatePostPage'
+import { CreatePostPage } from './pages/CreatePostPage'
 import { CreateCommunityPage } from './pages/CreateCommunityPage'
 import { CommunitiesPage } from './pages/CommunitiesPage'
 import { CommunityPage } from './pages/CommunityPage'
+import { PostDetails } from './pages/PostDetails'
 
 
 function App() {
   return (
     <div className="min-h-screen bg-black text-gray-100 transition-opacity duration-700 pt-20">
-     <Navbar />
+      <Navbar />
       <div className="container mx-auto px-4 py-6 ">
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/create" element={<CreatPostPage />}/>
-          <Route path="/post:id" element={<CreatPostPage />}/>
-          <Route path="/community/create" element={<CreateCommunityPage />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePostPage />} />
+          <Route path="/post/:postId" element={<PostDetails />} />
+          <Route path="/community/create" element={<CreateCommunityPage />} />
           <Route path="/communities" element={<CommunitiesPage />} />
           <Route path="/community/:id" element={<CommunityPage />} />
         </Routes>
